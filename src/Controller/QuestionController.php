562e592b58ff153d1f1444a7d913c2f1a5c327cc
Marcel <m.j.vd.linden@rocmondriaan.nl>
2022-03-14 11:18:23 +0100

    pizza categories

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -9,12 +9,36 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
     }
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
+    }
+    /**
+     * @Route ("/pizza")
+     */
+    public function pizza()
+    {
+        $categories=['classics','vegan','premium','meat lovers'];
+        return $this->render('question/pizza.html.twig', ['categories'=>$categories]);
+    }
+
 }
